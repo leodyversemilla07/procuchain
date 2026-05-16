9dabf478fb04bfb00e0653736e1651166f606037
Leodyver S. Semilla <leodyversemilla07@gmail.com>
2026-05-16 11:40:56 +0000

    fix: correct namespace in HOPE check script

--- check_hope_subs.php
+++ check_hope_subs.php
@@ -5,7 +5,9 @@ require __DIR__.'/vendor/autoload.php';
 $app = require_once __DIR__.'/bootstrap/app.php';
 $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
 
-$client = new \MultiChain\Client('172.31.42.5', 6834, config('multichain.rpc_user'), config('multichain.rpc_password'), false);
+use App\Libraries\MultiChain\Client;
+
+$client = new Client('172.31.42.5', 6834, config('multichain.rpc_user'), config('multichain.rpc_password'), false);
 $client->setoption('chain_name', config('multichain.chain_name'));
 
 $streams = ['procurement.metadata','procurement.status','procurement.documents','procurement.corrections','procurement.metadata.corrections','procurement.archive','procurement.events','file.data','file.metadata','file.chunks'];
