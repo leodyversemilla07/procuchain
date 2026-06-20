da9fd895839e5ed16dc329ed4e29b51fe557862a
Leodyver Semilla <leodyversemilla07@gmail.com>
2026-06-20 09:39:11 +0800

    chore: remove stale one-off scripts and plan.md

--- scripts/fix-mysql-user.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$pdo = new PDO('mysql:host=127.0.0.1;port=3306', 'root', '');
-$pdo->exec("CREATE USER IF NOT EXISTS 'procuchain'@'%' IDENTIFIED BY ''");
-$pdo->exec("GRANT ALL PRIVILEGES ON procuchain.* TO 'procuchain'@'%'");
-$pdo->exec('FLUSH PRIVILEGES');
-echo 'Done. ';
-$test = new PDO('mysql:host=127.0.0.1;port=3306;dbname=procuchain', 'procuchain', '');
-echo 'Connected as procuchain!';
